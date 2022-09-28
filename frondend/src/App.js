9980e7b545f4a5e58abfe703373914511da57205
andrerit <andrerit@stud.ntnu.no>
2022-09-28 21:02:56 +0200

    press room commit

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -30,7 +30,7 @@ export default function App() {
     '/thesis/:id' : (props) => <ThesisDetailsPage id={props.id} />,
     '/join' : () => <JoinPage />,
     '/admin' : () => <AdminPage />,
-    '/contact' : () => <ContactPage />,
+    '/pressroom' : () => <ContactPage />,
   }
   
   const routeResult = useRoutes(routes);

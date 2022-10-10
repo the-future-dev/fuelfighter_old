6b43bc6dee4c1669f1d7b6d3bd65cebc7e8e7fe5
Olav Larsen Halleraker <olavhalleraker@dhcp-10-22-125-65.wlan.ntnu.no>
2022-10-10 18:23:03 +0200

    Added Key People and changed name from Press Room to Contact Us

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -30,7 +30,7 @@ export default function App() {
     '/thesis/:id' : (props) => <ThesisDetailsPage id={props.id} />,
     '/join' : () => <JoinPage />,
     '/admin' : () => <AdminPage />,
-    '/pressroom' : () => <ContactPage />,
+    '/contact' : () => <ContactPage />,
   }
   
   const routeResult = useRoutes(routes);
--- frondend/src/pages/Contact page.js
+++ frondend/src/pages/Contact page.js
@@ -7,15 +7,15 @@ import '../_styles/contact.css'
 import Header from '../_templates/Header'
 import Footer from '../_templates/Footer'
 
-import robert from '../assets/contact/robertBlas.png'
-import frida from '../assets/contact/frida.jpg'
+import logo from '../assets/logo_black.png'
+import pictureMissing from '../assets/team/default.png'
 
 // Assets
 import {name} from '../components/name';
 
-const projectLeader = {name: "Tor Børve Rasmussen", email: "tors...",};
-const sponsorLeader = {name: "abc..", email: "abc.."};
-const marketingLeader = {name: "", email: ""}
+// const projectLeader = {name: "Tor Børve Rasmussen", email: "tors...",};
+// const sponsorLeader = {name: "abc..", email: "abc.."};
+// const marketingLeader = {name: "", email: ""} 
 
 export default function contactpage() {
 
@@ -26,31 +26,62 @@ export default function contactpage() {
                     <div className="grid">
                         <h1> Contact {name} </h1>
                         <p> Any questions? Don't hesitate to contact us! </p> 
+                        <div className="container">
+                            <div className="contactCard">
+                                <div className="infos">
+                                    <h4 id="generalPositionContact"> General inquiries</h4>
+                                    <h5 id="generalDetailsContact"> Email: <a href="mailto:contact@fuelfighter.no" >contact@fuelfighter.no</a></h5>
+                                </div>
+                                <img id="contactIMG" alt="logo " src={logo}/>
+                            </div>
+                        </div>   
                         
-                    
+
+                        <h2 id="keyPeople"> Key people </h2>
+
                         <div className="container">
                             <div className='contactCard'>
                                 <div className='infos'>
-                                    <h3>Robert Blaszkiewic</h3>
-                                    <h4>Sponsorship Manager</h4>
-                                    <h5 id="detailsContact">Tll:        <a href="tell:94131904" >94131904</a></h5>
-                                    <h5 id="detailsContact">Email:      <a href="mailto:br@fuelfighter.no" >br@fuelfighter.no</a></h5>
+                                    <h3>Tor Børve Rasmussen</h3>
+                                    <h4>Project Manager</h4>
+                                    <h5 id="detailsContact">Tlf:        <a href="tell:94131904" >941 34 655</a></h5>
+                                    <h5 id="detailsContact">Email:      <a href="mailto:br@fuelfighter.no" >leder@fuelfighter.no</a></h5>
                                 </div>
-                                <img id="contactIMG" alt="robert " src={robert}/>
+                                <img id="contactIMG" alt="Picture missing " src={pictureMissing}/> 
+                            </div>
+
+                            <div className='contactCard'>
+                                <div className='infos'>
+                                    <h3>Steve Carter Feujo Nomeny</h3>
+                                    <h4>Technical leader</h4>
+                                    <h5 id="detailsContact">Tlf:        <a href="tell:45513404" >969 95 644</a></h5>
+                                    <h5 id="detailsContact">Email:      <a href="mailto:teknisk@fuelfighter.no" >teknisk@fuelfighter.no</a></h5>
+                                </div>
+                                <img id="contactIMG" alt="Picture missing " src={pictureMissing}/>
                             </div>
                         </div>
+
                         <div className="container">
                             <div className='contactCard'>
                                 <div className='infos'>
-                                    <h3>Frida Nymark Englestad</h3>
-                                    <h4>Sponsorship Manager</h4>
-                                    <h5 id="detailsContact">Tll:        <a href="tell:45513404" >45513404</a></h5>
-                                    <h5 id="detailsContact">Email:      <a href="mailto:nestleder@fuelfighter.no" >nestleder@fuelfighter.no</a></h5>
+                                    <h3>Yao Bin David Zhang</h3>
+                                    <h4>Marketing leader</h4>
+                                    <h5 id="detailsContact">Tlf:        <a href="tell:902 21 802" >902 21 802</a></h5>
+                                    <h5 id="detailsContact">Email:      <a href="mailto:david.zhang@fuelfighter.no" >david.zhang@fuelfighter.no</a></h5>
                                 </div>
-                                <img id="contactIMG" alt="frida " src={frida}/>
+                                <img id="contactIMG" alt="Picture missing " src={pictureMissing}/>
                             </div>
-                        </div>
-                        
+
+                            <div className='contactCard'>
+                                <div className='infos'>
+                                    <h3>Adrian Golamini</h3>
+                                    <h4>Sponsorship Manager</h4>
+                                    {/* <h5 id="detailsContact">Tlf:        <a href="tell: ?" >???</a></h5> */}
+                                    <h5 id="detailsContact">Email:      <a href="mailto:adrian.golamini@fuelfighter.no" >adrian.golamini@fuelfighter.no</a></h5>
+                                </div>
+                                <img id="contactIMG" alt="Picture missing " src={pictureMissing}/>
+                            </div>
+                        </div>                        
 
                         {/* <p> For sponsor related inquiries, please contact ... </p>
 
